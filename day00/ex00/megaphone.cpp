05b8999f151441dd21cc8a41e21fb33a51c725d5
Blue-bg <82770859+Blue-bg@users.noreply.github.com>
2022-10-05 10:54:15 +0200

    Ex00
    finish Megaphone

---
+++ day00/ex00/megaphone.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: amiguez <amiguez@student.42lyon.fr>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/09/20 01:13:32 by amiguez           #+#    #+#             */
+/*   Updated: 2022/09/20 01:13:56 by amiguez          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(int argc, char **argv)
+{
+	int	i;
+	int	j;
+
+	i = 1;
+	if (argc == 1)
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	while (i < argc)
+	{
+		j = 0;
+		while (argv[i][j])
+		{
+			std::cout << (char)toupper(argv[i][j]);
+			j++;
+		}
+		i++;
+	}
+	std::cout << std::endl;
+	return (0);
+}
